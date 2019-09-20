562e3fd86c1a0552e08a98e600a84dc2a9e606b0
farshid inanloo <farshidinanloo73@gmail.com>
2019-09-20 13:19:30 +0430

    Updates

---
+++ precache-manifest.5faa9be7b24986d476d191d5008de4de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde5992c2bf045d35fc9bd5a171a674d",
+    "url": "/React-Shopping-Store/index.html"
+  },
+  {
+    "revision": "4ff062e6e1a032f0833b",
+    "url": "/React-Shopping-Store/static/css/main.b5343904.chunk.css"
+  },
+  {
+    "revision": "468e59c7b9f5fbf64661",
+    "url": "/React-Shopping-Store/static/js/2.5fd0d6b7.chunk.js"
+  },
+  {
+    "revision": "4ff062e6e1a032f0833b",
+    "url": "/React-Shopping-Store/static/js/main.ac6f81a2.chunk.js"
+  },
+  {
+    "revision": "994d58b6b30e691ef226",
+    "url": "/React-Shopping-Store/static/js/runtime~main.9c5c854d.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/React-Shopping-Store/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/React-Shopping-Store/static/media/shopping-bag.30f07071.svg"
+  }
+]);
